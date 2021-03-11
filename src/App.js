1ce1dea17920e0fcabed4abe75e612f0a211ecbb
Zohan Momin <zohanaiderak@gmail.com>
2021-03-11 08:28:47 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  state = {
+    location: ''
+  }
+
+  userInput = (e) => {
+    e.preventDefault();
+    let input = document.getElementById('searchInput').value;
+    axios(`https://www.metaweather.com/api/location/search/?query=${input}`,
+    {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+    })
+    .then(res => {
+      this.setState({location: res.data})
+      console.log(res.data)
+    })
+    .catch(err => alert(err))
+    console.log(this.state.location)
+  }
+
+  output = () => {
+    return console.log(this.state.location);
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="searchInput"></input>
+      <button onClick={this.userInput}>Search</button>
+      <h2>{this.output}</h2>
     </div>
   );
+  }
 }
 
 export default App;
